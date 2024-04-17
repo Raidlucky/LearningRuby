97ff40b1f95cab1f116f3e94972c734ef257c148
Lucky <68414136+Raidlucky@users.noreply.github.com>
2024-04-17 14:01:22 -0600

    Initial commit

---
+++ notes.rb
@@ -0,0 +1,15 @@
+my_num = 15
+my_boolean = true
+my_string = "Ruby"
+
+puts my_num
+puts my_boolean
+puts my_string
+
+if my_num > 15
+  puts "number is greater than 15"
+elsif my_num < 15
+  puts "my number is less than 15"
+elsif my_num == 15
+  puts "number is equal to 15"
+end
